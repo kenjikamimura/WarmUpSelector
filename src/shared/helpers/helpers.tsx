16714ce963b539f25437f9af2bc9c35925d2c2fb
Kenji Kamimura <kjp.kamimura@gmail.com>
2019-09-01 16:18:01 +1200

    Make the piece sorting by playingOrder not bookOrder

--- src/shared/helpers/helpers.tsx
+++ src/shared/helpers/helpers.tsx
@@ -4,10 +4,10 @@ export const compareSongs = (a: ISong, b: ISong) => {
   if (a === null || b === null) {
     return 0;
   }
-  if (a.bookOrder < b.bookOrder) {
+  if (a.playingOrder < b.playingOrder) {
     return -1;
   }
-  if (a.bookOrder > b.bookOrder) {
+  if (a.playingOrder > b.playingOrder) {
     return 1;
   }
   return 0;
