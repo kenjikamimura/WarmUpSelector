58afd31ddb727313b8cf55c1c4725fe22d67f7eb
KenjiKamimura <kjp.kamimura@gmail.com>
2020-08-01 15:01:17 +1200

    Convert song list from using table component to list to look nicer

--- src/components/SelectedPieces.tsx
+++ src/components/SelectedPieces.tsx
@@ -1,26 +1,16 @@
 import React from "react";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableRow,
   Paper,
+  List,
+  ListItemAvatar,
+  ListItem,
+  ListItemText,
+  Divider,
 } from "@material-ui/core";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
 import ISong from "../shared/interfaces/ISong";
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: "100%",
-      marginTop: theme.spacing(3),
-      overflowX: "auto",
-      display: "flex",
-      justifyContent: "center",
-    },
-  })
-);
 
 interface IProps {
   selectedPieces: ISong[];
@@ -31,39 +21,70 @@ export default function SelectedPieces({ selectedPieces, reset }: IProps) {
   console.log(selectedPieces);
   const classes = useStyles();
 
-  const emptyTableRow = (
-    <TableRow>
-      <TableCell component="th" scope="row" align="center">
-        {"---"}
-      </TableCell>
-    </TableRow>
+  const emptyListItem = (
+    <ListItem className={classes.listItem}>
+      <ListItemAvatar>
+        <ChevronRightIcon />
+      </ListItemAvatar>
+    </ListItem>
   );
 
   return (
     <Paper className={classes.root}>
-      <Table>
-        <TableBody>
-          {reset || !selectedPieces.length ? (
-            <>
-              {emptyTableRow}
-              {emptyTableRow}
-              {emptyTableRow}
-            </>
-          ) : (
-            selectedPieces.map((piece) => {
-              return piece ? (
-                <TableRow key={piece.playingOrder + "." + piece.name}>
-                  <TableCell component="th" scope="row" align="center">
-                    {piece.bookOrder + ". " + piece.name}
-                  </TableCell>
-                </TableRow>
-              ) : (
-                emptyTableRow
-              );
-            })
-          )}
-        </TableBody>
-      </Table>
+      <List className={classes.list}>
+        {reset || !selectedPieces.length ? (
+          <>
+            {emptyListItem}
+            <Divider variant="fullWidth" component="hr" light />
+            {emptyListItem}
+            <Divider variant="fullWidth" component="hr" light />
+            {emptyListItem}
+          </>
+        ) : (
+          selectedPieces.map((piece, index) => {
+            return piece ? (
+              <>
+                <ListItem
+                  className={classes.listItem}
+                  key={piece.playingOrder + "." + piece.name}
+                >
+                  <ListItemAvatar>
+                    <ChevronRightIcon />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={piece.name}
+                    secondary={`Book. ${piece.booknumber}, No. ${piece.bookOrder}`}
+                  ></ListItemText>
+                </ListItem>
+                {index < selectedPieces.length - 1 && (
+                  <Divider variant="fullWidth" component="hr" light />
+                )}
+              </>
+            ) : (
+              <>
+                {emptyListItem}
+                <Divider variant="fullWidth" component="hr" light />
+              </>
+            );
+          })
+        )}
+      </List>
     </Paper>
   );
 }
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      marginTop: theme.spacing(3),
+      overflowX: "auto",
+      display: "flex",
+      justifyContent: "center",
+    },
+    list: { width: "100%", padding: 0 },
+    listItem: {
+      height: "4rem",
+    },
+  })
+);
